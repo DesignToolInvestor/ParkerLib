9276368e056a9b056c12677e5774c4be41adc915
Kenneth W. Parker <47489615+DesignToolInvestor@users.noreply.github.com>
2023-03-27 17:15:48 -0400

    First check in

--- OldRand.h
+++ OldRand.h
@@ -1,5 +1,10 @@
 /*
   O l d R a n d . h
+
+
+  This is a unit for old fashion RNG (Random Number Generators), i.e., simple seed sequence 
+  generators without shuffle.  These are no longer acquitted for most large scale simulations
+  but they are useful for testing.
 */
 
 #pragma once
