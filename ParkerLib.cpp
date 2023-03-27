897c06bec2ea090b4ed5b8ecae53697f12be3e01
Kenneth W. Parker <47489615+DesignToolInvestor@users.noreply.github.com>
2023-03-27 17:15:31 -0400

    First check in

---
+++ ParkerLib.cpp
@@ -0,0 +1,14 @@
+/*
+  P a r k e r L i b . c p p
+
+  This file is required by the VS (Visual Studio) project type, but is not used because this is 
+  really a library project.  But the command line project type was used because I hadn't yet made
+  any of the library type files work for what are essentially just template header files.
+*/
+
+#include <iostream>
+
+int main()
+{
+  std::cout << "Hello World!\n";
+}
